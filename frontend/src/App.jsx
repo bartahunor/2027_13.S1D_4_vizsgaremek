8e9e4acdb6c6498b8c2487aca7ba3c1fc4a57ad5
bartahunor <barta.hunor.attila@diak.szbi-pg.hu>
2026-09-14 17:21:18 +0200

    login page pt1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import HomePage from './components/pages/HomePage'
@@ -7,13 +8,25 @@ import RegisterPage from './components/pages/RegisterPage'
 import TaskSelection from './components/pages/TaskType'
 
 export default function App() {
+  const [token, setToken] = useState(false);
+  if (token) {
+    sessionStorage.setItem('token', JSON.stringify(token));
+  }
+
+  useEffect(() => {
+    if (sessionStorage.getItem('token')) {
+      let data = JSON.parse(sessionStorage.getItem('token'));
+      setToken(data);
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <Navbar />
       <main className="min-h-screen">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage setToken={setToken} />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/tasktype" element={<TaskSelection />} />
         </Routes>
